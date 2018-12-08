76a3637977046ba7464c9b7546451cd156ee1908
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-12-08 11:14:40 +0100

    Setup working html application

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
 module.exports = {
-  entry: "./client/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve("dist"),
     filename: "index_bundle.js"
   },
+  mode: "development",
   module: {
     rules: [
       {
@@ -23,5 +31,6 @@ module.exports = {
         use: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
