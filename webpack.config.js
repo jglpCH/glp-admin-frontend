5ef811715b957398566b4cbd2333d487f22260ea
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-12-15 18:02:41 +0100

    Configure simpler setup for eslint/prettier

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,35 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: "./src/index.html",
-  filename: "index.html",
-  inject: "body"
+    template: "./src/index.html",
+    filename: "index.html",
+    inject: "body"
 });
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve("dist"),
-    filename: "index_bundle.js"
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      }
-    ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("dist"),
+        filename: "index_bundle.js"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
 };
